18d014b8dacb58330d5bca98a696106e14adeb72
Farshad-Davari <farshaddavari444@gmail.com>
2024-06-03 00:48:56 -0700

    requirements has installed

---
+++ main.py
@@ -0,0 +1,8 @@
+import os 
+import tensorflow as tf 
+import numpy as np 
+import seaborn as sns 
+import pathlib 
+from IPython import display 
+from matplotlib import pyplot as plt
+from sklearn.metrics import classification_report
